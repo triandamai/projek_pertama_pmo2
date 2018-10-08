4f6711c90aa29165dd44ddfb0aa691c914cb033f
unknown <triannurizkillah@gmail.com>
2018-10-08 06:15:18 -0700

    pmo2_sederhana

---
+++ app/src/main/java/com/example/kelompok3/projek_sederhana_pmo2_2/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kelompok3.projek_sederhana_pmo2_2;
+
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Locale;
+
+//author trian damai
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+
+        String lang = settings.getString("LANG", "");
+        if (!"".equals(lang) && !config.locale.getLanguage().equals(lang)) {
+            Locale locale = new Locale(lang);
+            Locale.setDefault(locale);
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+}
+}
